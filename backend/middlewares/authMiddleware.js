d686271da7fa7c29c9e6f272173c6365bb9036bc
priyankak3 <itspriyanka.kadam@spit.ac.in>
2025-05-06 19:01:40 +0530

    authmiddleware

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -9,8 +9,9 @@ const verifyAdmin = (req, res, next) => {
     }
 
     const token = authHeader.split(" ")[1];
+    jwtString = jwtString.replace(/^"|"$/g, "");
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    
+
     if (decoded.role !== "admin") {
       return res.status(403).json({ message: "Forbidden: Admins only" });
     }
